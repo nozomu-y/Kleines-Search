f1b9c81e462f49ebcedf24776dded4b08623dce9
nozoyama39 <nozoyama39@gmail.com>
2020-03-12 19:07:46 +0900

    Create config_template.php

---
+++ core/config_template.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ * Template for config.php
+ * Edit and save this file as config.php
+ * 
+ * Configuration for database connection
+ */
+
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
