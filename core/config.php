fa2fd475573ab9595fbbb519d114aedc84c4cd3c
nozoyama39 <nozoyama39@gmail.com>
2020-03-12 18:42:26 +0900

    Update config.php

---
+++ core/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ */
+
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
