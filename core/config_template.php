f1933db2d8535afd8def45f76303cf0001236606
nozoyama39 <nozoyama39@gmail.com>
2020-03-12 19:03:53 +0900

    init

---
+++ core/config_template.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ * Template for config.php
+ * edit and save this file as config.php
+ */
+
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
---
+++ create_database.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ */
