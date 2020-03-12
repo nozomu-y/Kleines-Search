c9c2d24da3c2af67e9bfaa9f6614e083d66f3206
nozoyama39 <nozoyama39@gmail.com>
2020-03-12 19:37:08 +0900

    Create create_table.php

---
+++ create_table.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ * Create tables on initialization
+ */
+
+require_once('./core/dbconnect.php');
