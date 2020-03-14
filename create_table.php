d24615db9b4ba66f1a758a9d77ac864793a00f2c
nozoyama39 <nozoyama39@gmail.com>
2020-03-14 23:02:11 +0900

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -10,7 +10,16 @@
 require_once(__DIR__ . '/core/dbconnect.php');
 
 // create table if it does not exist
-$query = "CREATE TABLE IF NOT EXISTS table_name(id int(6)";
+$query = "CREATE TABLE IF NOT EXISTS documents(id int(10), url varchar(256))";
+$result = $mysqli->query($query);
+if (!$result) {
+    print('Query failed on line ' . __LINE__ . ' in ' . __FILE__ . ': ' . $mysqli->error);
+    $mysqli->close();
+    exit();
+}
+$result->close();
+
+$query = "CREATE TABLE IF NOT EXISTS inverted_index(keyword varchar(256))";
 $result = $mysqli->query($query);
 if (!$result) {
     print('Query failed on line ' . __LINE__ . ' in ' . __FILE__ . ': ' . $mysqli->error);
