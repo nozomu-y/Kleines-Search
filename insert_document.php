e766ae6f2855b5d20ad314f8d278b6f59a5763df
nozoyama39 <nozoyama39@gmail.com>
2020-03-15 14:21:59 +0900

    initialize column with zeros

--- insert_document.php
+++ insert_document.php
@@ -39,6 +39,7 @@ function insert_document(string $url)
     $result->close();
     $doc_id = str_pad((string) $doc_id, 10, 0, STR_PAD_LEFT);
 
+    // check whether the column exists
     $query = "SHOW COLUMNS FROM inverted_index";
     $result = $mysqli->query($query);
     if (!$result) {
@@ -57,6 +58,7 @@ function insert_document(string $url)
             $col_exists = TRUE;
         }
     }
+    // add column if not exists
     if (!$col_exists) {
         $query = "ALTER TABLE inverted_index ADD COLUMN $doc_id int(5) DEFAULT 0";
         $result = $mysqli->query($query);
@@ -67,6 +69,16 @@ function insert_document(string $url)
         }
         $result->close();
     }
+
+    // initialize column with zero
+    $query = "UPDATE inverted_index SET $doc_id = 0";
+    $result = $mysqli->query($query);
+    if (!$result) {
+        print('<strong>Query failed:</strong> ' . $mysqli->error . 'thrown in <strong>' . __FILE__ . '</strong> on line <strong>' . __LINE__ . '</strong>');
+        $mysqli->close();
+        exit();
+    }
+    $result->close();
 
     $mysqli->close();
 }
