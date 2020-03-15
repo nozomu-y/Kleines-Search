eb93d8feaea5eca7929527a35b45906608ab53e6
nozoyama39 <nozoyama39@gmail.com>
2020-03-15 14:21:37 +0900

    add AUTO_INCREMENT to column id

--- initialize_table.php
+++ initialize_table.php
@@ -10,7 +10,7 @@
 require_once(__DIR__ . '/core/dbconnect.php');
 
 // create table if it does not exist
-$query = "CREATE TABLE IF NOT EXISTS documents(id int(10) UNSIGNED ZEROFILL PRIMARY KEY, url varchar(256))";
+$query = "CREATE TABLE IF NOT EXISTS documents(id int(10) UNSIGNED ZEROFILL PRIMARY KEY AUTO_INCREMENT, url varchar(256))";
 $result = $mysqli->query($query);
 if (!$result) {
     print('<strong>Query failed:</strong> ' . $mysqli->error . 'thrown in <strong>' . __FILE__ . '</strong> on line <strong>' . __LINE__ . '</strong>');
