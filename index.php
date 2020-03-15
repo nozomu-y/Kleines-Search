a221511f87ebc9f3bd835b31aa544a27c6c1e17e
nozoyama39 <nozoyama39@gmail.com>
2020-03-16 02:07:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+/*
+ * Copyright (c) 2020 Nozomu Yamazaki
+ * Released under the MIT license
+ * https://opensource.org/licenses/mit-license.php
+ * 
+ */
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
